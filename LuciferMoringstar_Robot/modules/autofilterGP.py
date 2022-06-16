6c02a3ca04c19ef4ee159900ccb19f63480f741b
Tr6666 <106578051+Tr6666@users.noreply.github.com>
2022-06-16 18:11:51 +0530

    Update autofilterGP.py

--- LuciferMoringstar_Robot/modules/autofilterGP.py
+++ LuciferMoringstar_Robot/modules/autofilterGP.py
@@ -87,8 +87,9 @@ async def group_filters(client, update):
             buttons.append([InlineKeyboardButton("📃 Pages 1/1",callback_data="pages"),
                             InlineKeyboardButton("Close 🗑️", callback_data="close")])
 
-            buttons.append([InlineKeyboardButton("🎬 𝗛𝗢𝗪 𝗧𝗢 𝗗𝗢𝗪𝗡𝗟𝗢𝗔𝗗 🎬", url=f"https://telegram.dog/{temp.Bot_Username}"),
-                            InlineKeyboardButton("💯 𝗨𝗡𝗔𝗕𝗟𝗘 𝗧𝗢 𝗔𝗖𝗖𝗘𝗦 𝗦𝗢𝗟𝗩𝗘𝗗 💯", url=f"https://droplink.co/TamilRoarsFiles")])
+            buttons.append([InlineKeyboardButton("🎬 𝗛𝗢𝗪 𝗧𝗢 𝗗𝗢𝗪𝗡𝗟𝗢𝗔𝗗 🎬", url=f"https://telegram.dog/{temp.Bot_Username}")])
+
+            buttons.append([InlineKeyboardButton("💯 𝗨𝗡𝗔𝗕𝗟𝗘 𝗧𝗢 𝗔𝗖𝗖𝗘𝗦 𝗦𝗢𝗟𝗩𝗘𝗗 💯", url=f"https://droplink.co/TamilRoarsFiles")])
 
             if settings["photo"]:
                 await client.send_photo(chat_id=update.chat.id, photo=random.choice(PICS), caption=MOVIE_TEXT.format(mention=update.from_user.mention, query=search, greeting=None, group_name = f"[{update.chat.title}](t.me/{update.chat.username})" or f"[{update.chat.title}](t.me/{update.from_user.username})"), reply_markup=InlineKeyboardMarkup(buttons), reply_to_message_id=update.id)
@@ -103,8 +104,9 @@ async def group_filters(client, update):
                         InlineKeyboardButton("🗑️", callback_data="close"),
                         InlineKeyboardButton("➡",callback_data=f"nextgroup_0_{keyword}")])
 
-        buttons.append([InlineKeyboardButton("🎬 𝗛𝗢𝗪 𝗧𝗢 𝗗𝗢𝗪𝗡𝗟𝗢𝗔𝗗 🎬", url=f"https://telegram.dog/{temp.Bot_Username}"),
-                        InlineKeyboardButton("💯 𝗨𝗡𝗔𝗕𝗟𝗘 𝗧𝗢 𝗔𝗖𝗖𝗘𝗦 𝗦𝗢𝗟𝗩𝗘𝗗 💯", url=f"https://droplink.co/TamilRoarsFiles")])
+        buttons.append([InlineKeyboardButton("🎬 𝗛𝗢𝗪 𝗧𝗢 𝗗𝗢𝗪𝗡𝗟𝗢𝗔𝗗 🎬", url=f"https://telegram.dog/{temp.Bot_Username}")])
+
+        buttons.append([InlineKeyboardButton("💯 𝗨𝗡𝗔𝗕𝗟𝗘 𝗧𝗢 𝗔𝗖𝗖𝗘𝗦 𝗦𝗢𝗟𝗩𝗘𝗗 💯", url=f"https://droplink.co/TamilRoarsFiles")])
 
         try:             
             if settings["photo"]:
