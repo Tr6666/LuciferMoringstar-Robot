9795bafa0dc46b731f3f83f0bd33ecb33a6f2cf7
Tr6666 <106578051+Tr6666@users.noreply.github.com>
2022-06-16 17:51:14 +0530

    Update callback.py

--- LuciferMoringstar_Robot/callback.py
+++ LuciferMoringstar_Robot/callback.py
@@ -251,7 +251,7 @@ async def cb_handler(bot, update):
                     try:
                         invite_link = await bot.create_chat_invite_link(GET_FILECHANNEL)      
                         dlFile = await bot.send_cached_media(chat_id=GET_FILECHANNEL, file_id=file_id, caption=caption, reply_markup=InlineKeyboardMarkup(buttons))
-                        dlReply = await update.message.reply_text(GETFILE_TEXT.format(mention=update.from_user.mention, file_name=title, file_size=size), reply_markup=InlineKeyboardMarkup( [[ InlineKeyboardButton("🎬 ᴅᴏᴡɴʟᴏᴀᴅ 🎬", url=dlFile.link) ],[ InlineKeyboardButton("👉 ᴄᴀɴ'ᴛ ᴅᴏᴡɴʟᴏᴀᴅ ᴄʟɪᴄᴋ ʜᴇʀᴇ 👈", url=invite_link.invite_link) ]] ))
+                        dlReply = await update.message.reply_text(GETFILE_TEXT.format(mention=update.from_user.mention, file_name=title, file_size=size), reply_markup=InlineKeyboardMarkup( [[ InlineKeyboardButton("🎬 ᴅᴏᴡɴʟᴏᴀᴅ 🎬", url=dlFile.link) ],[ InlineKeyboardButton("👉 ᴄᴀɴ'ᴛ ᴅᴏᴡɴʟᴏᴀᴅ ᴄʟɪᴄᴋ ʜᴇʀᴇ 👈", url=https://du-link.in/st?api=aeebdb7ca0a0819ea9ae1b6177ea22de1ec52f88&url=https://t.me/+vxmnk-_3ISwxMjE1) ]] ))
                         await asyncio.sleep(1000)
                         await dlFile.delete()
                         await dlReply.delete()
